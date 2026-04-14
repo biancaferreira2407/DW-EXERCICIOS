f99cedfec004f882819004f0d1782fc3fda08378
biancaferreira2407 <biancaferreira@alunos.utfpr.edu.br>
2026-04-14 10:41:02 -0300

    Update estacionaII.js
    b1

--- 1bimestre/exercicio5-TarifaEstaciona II ( 2026-04-09 )/estacionaII.js
+++ 1bimestre/exercicio5-TarifaEstaciona II ( 2026-04-09 )/estacionaII.js
@@ -1,7 +1,7 @@
 function fcalc(){
     /* salvar inputs html */
-    let dataI = document.getElementById("datahoraI").value;
-    let dataF = document.getElementById("datahoraF").value;
+    let dataI = document.getElementById("datahoraI").value
+    let dataF = document.getElementById("datahoraF").value
     let h1 = new Date(dataI);
     let hF = new Date(dataF);
     let veiculo = document.getElementById("veiculo");
@@ -10,6 +10,14 @@ function fcalc(){
     let mensagem = document.getElementById("mensagem");
      
     /* verificar erro de dataI > dataF */
+    if(dataF==="" || dataI===""){
+        footer.classList.add("erro")
+        mensagem.innerHTML="informe as datas de entrada e saída"
+        document.getElementById("datahoraI").focus()
+        document.getElementById("resp").innerHTML = "-";
+        document.getElementById("respH").innerHTML = "-";
+        return
+    }
     if(h1>hF){
         footer.classList.add("erro")
         mensagem.innerHTML="data e hora de saída tem que ser depois da data e hora de entrada"
@@ -18,23 +26,17 @@ function fcalc(){
         document.getElementById("respH").innerHTML = "-";
         return
     }
-    if(dataF==="" || dataI===""){
-        footer.classList.add("erro")
-        mensagem.innerHTML="informe as datas de entrada e saída"
-        document.getElementById("datahoraF").focus()
-        document.getElementById("resp").innerHTML = "-";
-        document.getElementById("respH").innerHTML = "-";
-        return
-    }
+    
 
 
     let horas = (hF - h1)/(1000*60*60);    /* tansforma dataI e dataF em horas */
 
 
     /* se a quantidade de horas for quebrada, apartir de 20 min arredonda para cima( 1h a mais) */
-    let decimal_min = horas - Math.floor(horas)
+    let aux = Math.floor(horas)
+    let decimal_min = horas - aux
     let hora;
-    if(decimal_min < 0.2){
+    if(decimal_min < 0.20){
         hora = Math.floor(horas)
     }else{
         hora = Math.ceil(horas)
@@ -44,24 +46,22 @@ function fcalc(){
         hora = 1;
     }
 
-   
+   let valorfixo;
 
-    let valorfixo = 5 + (2.5 * (hora - 1));    /* valor que será usado para o denconto ou acréssimo */
-   
     /* horas maiores que 24 */
     if(hora>=24){
         let h24 = Math.floor(hora/24)
         if(hora%24===0){
-            valorfixo = 60 * (hora/24)
+            valorfixo = 60 * h24
         }else{
             let resto = hora % 24;
             valorfixo = (60 * h24) +(2.5 * resto );
         } 
+    }else{
+         valorfixo = 5 + (2.5 * (hora - 1));    /* valor que será usado para o denconto ou acréssimo */
     }
- 
-    
 
-
+   
     /*calculo de acréssimo ou desconto*/
     let acressCarro = 1;
     let descClient = 1;
@@ -72,10 +72,9 @@ function fcalc(){
     if(veiculo.checked){            
         acressCarro = 1.25;
     }  
-        
-    
+     
     let valor = (valorfixo * acressCarro) * descClient;  /* calculo final da tarifa */
-
+    
    
     footer.classList.remove("erro")
     mensagem.innerHTML="Calculo realizado com sucesso"
