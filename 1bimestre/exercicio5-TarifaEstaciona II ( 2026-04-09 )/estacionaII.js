e62289090e52239b42402d16f9e4f0f953e49c2e
biancaferreira2407 <biancaferreira@alunos.utfpr.edu.br>
2026-04-13 23:27:44 -0300

    Update estacionaII.js
    Update estacionaII.js

--- 1bimestre/exercicio5-TarifaEstaciona II ( 2026-04-09 )/estacionaII.js
+++ 1bimestre/exercicio5-TarifaEstaciona II ( 2026-04-09 )/estacionaII.js
@@ -5,24 +5,45 @@ function fcalc(){
     let cliente = document.getElementById("cliente");
     let footer = document.getElementById("footer");
     let mensagem = document.getElementById("mensagem");
-    
-    let hora = Math.ceil((hF - h1)/(1000*60*60))
+     
+    if(h1>hF){
+        footer.classList.add("erro")
+        mensagem.innerHTML="data e hora de saída tem que ser depois da data e hora de entrada"
+        document.getElementById("datahoraF").focus()
+        document.getElementById("resp").innerHTML = "-";
+        document.getElementById("respH").innerHTML = "-";
+        return
+    }
+
+    let horas = (hF - h1)/(1000*60*60);
+
+
+    let decimal = horas - Math.floor(horas)
+    let hora;
+    if(decimal < 0.2){
+        hora = Math.floor(horas)
+    }else{
+        hora = Math.ceil(horas)
+    }
+    if(hora <= 0){
+        hora = 1;
+    }
 
     let valorfixo = 5 + (2.5 * (hora - 1));
 
-    let acressCarro = 1;
-    let descClient = 1;
-
     if(hora>=24){
-        let h24= parseInt(hora/24)
+        let h24 = Math.floor(hora/24)
         if(hora%24===0){
             valorfixo = 60 * (hora/24)
         }else{
-
-            valorfixo = (60 * (h24)) + (2.5 * h24 )
+            let resto = hora % 24;
+            valorfixo = (60 * h24) + (5 + (2.5 * (resto - 1)));
         } 
     }
 
+
+    let acressCarro = 1;
+    let descClient = 1;
     if(cliente.checked){
         descClient = 0.95;
     }
@@ -31,19 +52,13 @@ function fcalc(){
         acressCarro = 1.25;
     }  
         
-       
     
     let valor = (valorfixo * acressCarro) * descClient;  
-    
 
-    if (hora === ""|| isNaN(hora) || hora<0) {
-       footer.classList.add("erro")
-        mensagem.innerHTML="a quantidade de horas estacionadas precisa ser preenchida por um número positivo"
-        document.getElementById("hora").focus()
-        document.getElementById("resp").innerHTML = "-"
-    }else{
-         footer.classList.remove("erro")
-        mensagem.innerHTML="Calculo realizado com sucesso"
-        document.getElementById("resp").innerHTML = valor.toFixed(2);
-    }
+   
+    footer.classList.remove("erro")
+    mensagem.innerHTML="Calculo realizado com sucesso"
+    document.getElementById("respH").innerHTML = hora + " hora(s)";
+    document.getElementById("resp").innerHTML = valor.toFixed(2);
+    
 }
